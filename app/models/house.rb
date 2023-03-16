39941e60f3e87094dfd2bdee16dd8e5d637a1d36
Heber Lazo <heberlazo@gmail.com>
2023-03-16 08:34:32 -0500

    Corrected update of message

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,4 +2,9 @@ class House < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name
+
+
+  after_create_commit{ broadcast_prepend_to 'houses'}
+  after_update_commit{ broadcast_prepend_to 'houses'}
+
 end
