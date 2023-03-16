51f745c43c6c655da0eb5c252a579e44356adee9
Heber Lazo <heberlazo@gmail.com>
2023-03-15 21:07:23 -0500

    form completed

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,5 +5,20 @@ class HousesController < ApplicationController
   end
 
   def create
+    @house = current_user.build_house(house_params)
+
+    if @house.save
+      p @house
+    else
+      p @house.errors.full_messages
+    end
+
+  end
+
+
+  private
+
+  def house_params
+    params.require(:house).permit(:name)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,17 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def blue_btn
+    'btn btn-primary'
+  end
+
+  def green_btn
+    'btn btn-success'
+  end
+
+  def red_btn
+    'btn btn-danger'
+  end
+
 end
