c03faf63bcd0104a4a156c88c03b2d932d21babc
Heber Lazo <heberlazo@gmail.com>
2023-03-18 22:25:25 -0500

    Bug with broadcast

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,5 +3,5 @@ class Player < ApplicationRecord
   belongs_to :game
 
   # after_create_commit{ broadcast_prepend_to 'players', partial: 'games/players', locals: { player: self }, target: 'players' }
-
+  after_update_commit { broadcast_update_to game, :players }
 end
