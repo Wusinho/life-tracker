f5f3469f412747701f76f9a0f4ed2af28c1da71c
Heber Lazo <heberlazo@gmail.com>
2023-03-18 19:15:07 -0500

    removed unnecessary code

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,6 +1,5 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    p 'SUBSCRIBED'
     return unless current_user && params[:user_id]
 
     stream_from "game_channel_#{params[:user_id]}"
