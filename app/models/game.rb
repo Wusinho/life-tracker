1005c6066fcddf4cef5671229440c77e3fafb8b3
Heber Lazo <heberlazo@gmail.com>
2023-03-25 13:15:29 -0500

    refactor game_broadcast

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,17 +4,6 @@ class Game < ApplicationRecord
   accepts_nested_attributes_for :players, allow_destroy: true, reject_if: :all_blank
 
   after_create :add_game
-  # after_update_commit { broadcast_update_to game, :players }
-
-  def add_game
-    self.players.each do |player|
-      player.user.update(total_games: player.user.total_games + 1)
-      ActionCable.server.broadcast "game_channel_#{player.user_id}", {
-        user_id: player.user_id,
-        element: ApplicationController.render(partial: 'games/game', locals: { game: self })
-      }
-    end
-
-  end
+  after_create_commit { broadcast_prepend_to 'games'}
 
 end
