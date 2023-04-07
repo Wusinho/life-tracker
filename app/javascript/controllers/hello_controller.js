d92d6d4d3cadac5768d59cbc3d61a902722f9d4e
H Lazo <67179149+Wusinho@users.noreply.github.com>
2023-04-07 10:48:12 -0500

    Dev error msg animation (#6)
    * corrected error on refresh
    
    * refactor navbar data-controller
    
    * Added animation to errors
    
    * Added background color transition

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -3,21 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     window.onbeforeunload = () => {
-      const csrtToken = document.querySelector('meta[name="csrf-token"]').content
-      fetch('/windows_close', {
-        method: 'Get',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': csrtToken
-        },
+      document.addEventListener('keydown', function (e){
+        if (e.key.toString() !== 'F5') {return}
 
-      }).then(r => r.text())
-          .then(html =>{
-
-          } )
+        const csrtToken = document.querySelector('meta[name="csrf-token"]').content
+        fetch('/windows_close', {
+          method: 'Get',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': csrtToken
+          },
+        }).then(r => r.text())
+            .then(html =>{})
+      })
     };
   }
 }
