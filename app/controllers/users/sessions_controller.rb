f14795d0af972c9937fcf1e8063c42b957f331ec
H Lazo <67179149+Wusinho@users.noreply.github.com>
2023-04-07 11:26:05 -0500

    Dev hot fix (#7)
    * session create fixed bug
    
    * session create fixed bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  before_action :update_online_status, only: [:create, :destroy]
+  before_action :update_online_status, only: [:destroy]
+  after_action :update_online_status, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
